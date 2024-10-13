f864aeb568b0a1c8da3f8749d8779d2cafccadef
Ivan <steel-97@mail.ru>
2024-10-13 15:04:08 +0300

    Lint

--- src/ui/ui_bip32_path.h
+++ src/ui/ui_bip32_path.h
@@ -8,12 +8,12 @@
 typedef void (*ui_bip32_approve_callback)(void*);
 
 const ux_flow_step_t* ui_bip32_path_screen(uint32_t* path,
-                                     uint8_t path_len,
-                                     const char* title,
-                                     char* buffer,
-                                     uint8_t buffer_len,
-                                     ui_bip32_approve_callback cb,
-                                     void* cb_context);
+                                           uint8_t path_len,
+                                           const char* title,
+                                           char* buffer,
+                                           uint8_t buffer_len,
+                                           ui_bip32_approve_callback cb,
+                                           void* cb_context);
 
 #endif
 
