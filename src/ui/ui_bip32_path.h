1b0ff7b816e90c70f9f575067d1d58bf11523f9e
Ivan <steel-97@mail.ru>
2024-10-13 15:03:24 +0300

    Revert

--- src/ui/ui_bip32_path.h
+++ src/ui/ui_bip32_path.h
@@ -7,7 +7,7 @@
 
 typedef void (*ui_bip32_approve_callback)(void*);
 
-ux_flow_step_t* ui_bip32_path_screen(uint32_t* path,
+const ux_flow_step_t* ui_bip32_path_screen(uint32_t* path,
                                      uint8_t path_len,
                                      const char* title,
                                      char* buffer,
